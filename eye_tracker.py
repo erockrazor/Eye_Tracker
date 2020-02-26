61123d424d5576d3d4ed9ec43d9f5ae34f8f79ec
Eric P. Gieg <erockusd@Hotmail.com>
2020-02-25 22:04:38 -0500

    fix: adjusting eye frames

--- eye_tracker.py
+++ eye_tracker.py
@@ -16,54 +16,87 @@ def face_detection(img):
     gray_picture = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = FACE_CASCADE.detectMultiScale(gray_picture, 1.05, 5)
     if faces is not None:
-        # print "Face: " + faces
         for (x, y, w, h) in faces:
             frame = img[y:y + h, x:x + w]  # Remove the background
             # Draw Rectangle around Face
             img = cv2.rectangle(
                 img, (x, y), (x + w, y+(h//2)), (255, 255, 0), 3)
-        return frame
+            return frame
+    else:
+        return img
 
 
 def left_eye_detection(img):
     eyes = LEFT_EYE_CASCADE.detectMultiScale(img, 1.05, 5)
     if eyes is not None:
-        # print "Eyes: " + eyes
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 255), 3)
+            ey = adjust_eye_position(ey, eh)
+            eh = adjust_eye_height(eh)
+            frame = img[ey:ey + eh, ex:ex + ew]
+            cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 255), 1)
+            return frame
 
 
 def right_eye_detection(img):
     eyes = RIGHT_EYE_CASCADE.detectMultiScale(img, 1.05, 5)
     if eyes is not None:
-        # print "Eyes: " + eyes
+        print(eyes)
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (155, 0, 155), 3)
+            ey = adjust_eye_position(ey, eh)
+            eh = adjust_eye_height(eh)
+            frame = img[ey:ey + eh, ex:ex + ew]
+            cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (155, 0, 155), 1)
+            return frame
 
+def adjust_eye_height(eh):
+    return int(round(eh // 2))
+    
+
+def adjust_eye_position(ey, eh):
+    return int(round(ey + (eh / 2.5)))
+
+def blob_process(img, threshold, detector):
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
+    img = cv2.erode(img, None, iterations=2)
+    img = cv2.dilate(img, None, iterations=4)
+    img = cv2.medianBlur(img, 5)
+    keypoints = detector.detect(img)
+    print(keypoints)
+    return keypoints
 
 def nothing(x):
     pass
 
 
 def main():
-    cap = cv2.VideoCapture(0)
-    cv2.namedWindow('Eye Mouse', cv2.WINDOW_KEEPRATIO)
-    # cv2.createTrackbar('threshold', 'Eye Mouse', 36, 255, nothing)
-    cv2.createTrackbar('Frame Rate', 'Eye Mouse', 1, 10, nothing)
-    while True:
-        cv2.waitKey(cv2.getTrackbarPos('Frame Rate', 'Eye Mouse') * 100)
-        _, frame = cap.read()
-        face_frame = face_detection(frame)
-        # if face_frame is not None:
-        #     right_eye_frame = right_eye_detection(face_frame)
-        #     if right_eye_frame is not None:
-        #         print('found right eye')
-            
-        #     left_eye_frame = left_eye_detection(face_frame)
-        #     if right_eye_frame is not None:
-        #         print('found right eye')
 
-        cv2.imshow('Eye Mouse', frame)
+    cap = cv2.VideoCapture(0)
+    # cv2.namedWindow('Eye Mouse', cv2.WINDOW_KEEPRATIO)
+    cv2.namedWindow('Eye Mouse')
+    cv2.createTrackbar('threshold', 'Eye Mouse', 36, 255, nothing)
+    # cv2.createTrackbar('Frame Rate', 'Eye Mouse', 1, 10, nothing)
+    img = cv2.imread('picture.jpg')
+
+    while True:
+        # cv2.waitKey(cv2.getTrackbarPos('Frame Rate', 'Eye Mouse') * 100)
+        threshold = r = cv2.getTrackbarPos('threshold', 'image')
+        # _, frame = cap.read()
+        face_frame = face_detection(img)
+        print(face_frame)
+        if face_frame is not None:
+            
+            right_eye_frame = right_eye_detection(face_frame)
+            if right_eye_frame is not None:
+                print('found right eye')
+                keypoints = blob_process(right_eye_frame, threshold, DETECTOR)
+                right_eye_frame = cv2.drawKeypoints(right_eye_frame, keypoints, right_eye_frame, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                cv2.imshow('Eye Mouse', right_eye_frame)
+
+            left_eye_frame = left_eye_detection(face_frame)
+            if left_eye_frame is not None:
+                print('found left eye')
+        # cv2.imshow('Eye_Mouse.png', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cv2.waitKey(0)
